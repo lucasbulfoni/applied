5c2a1cd4c0eae92d29840a8a62a4d60c19c4ded2
lucasbulfoni <33683219+lucasbulfoni@users.noreply.github.com>
2018-01-11 13:46:14 +0100

    maj 2.0

--- Applied.R
+++ Applied.R
@@ -1,19 +1,23 @@
 library(foreign)
 
 datademo <- read.spss("~/GitHub/Applied/2002 1 Demography.sav", to.data.frame=TRUE)
-datademo <- c(datademo$rbd, datademo$brojclan , datademo$a7 )
+datademo <- datademo[c(2,4,13)] 
+names(datademo)
 
 datahouse <- read.spss("~/GitHub/Applied/2002 2 Housing and durables.sav", to.data.frame=TRUE)
-datahouse <- c(datahouse$rbd , datahouse$s2 , datahouse$s3 , datahouse$s14_4(y))
+datahouse <- datahouse[c(2,5,6,7,43,23,59)]
+names(datahouse)
+
+
 
 dataincome <- read.spss("~/GitHub/Applied/2002 IncomeConsumption.sav", to.data.frame=TRUE)
-dataincome <-c(dataincome$rbd , dataincome$income )
-dataincome$rbd
-class(dataincome)
-class(datahouse)
-datademo$rbd
+dataincome <- dataincome[c(2,93)]
+names(dataincome)
+
+
+summary(datahouse)
+
 data <- merge(datademo,datahouse,by.x = "rbd" , by.y = "rbd")
-data <- merge(data,dataincome,by.x=data$rbd , by.y = dataincome$rdb)
+data <- merge(data,dataincome,by.x="rbd" , by.y = "rbd")
 
 names(datademo)
-summary(datahouse)
