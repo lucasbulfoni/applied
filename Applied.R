9be6129ccc90849afe215795384f0715d3af462b
DLachiver <33716514+DLachiver@users.noreply.github.com>
2018-02-08 16:32:25 +0100

    model log

--- Applied.R
+++ Applied.R
@@ -1,2 +1,11 @@
 library(readxl)
+
 table <- read_excel("~/GitHub/Applied/table.xlsx")
+table <- read_excel("C:/Users/Dimitri/Dropbox/[Cours]/Applied/R/applied/table.xlsx")
+
+View(table)
+
+attach(table)
+
+modelLog <- lm(Lconso ~ income + HousMembers + NumbRooms + FloorSpace + ElecYN + ElecCookYN )
+summary(modelLog)
