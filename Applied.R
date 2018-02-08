08168fc2f0e1cea13cf5a4eaf244d0f4ccc2d0fc
lucasbulfoni <33683219+lucasbulfoni@users.noreply.github.com>
2018-02-08 16:18:21 +0100

    maj

--- Applied.R
+++ Applied.R
@@ -1,31 +1,21 @@
 library(foreign)
 
-table <- read.csv(file=file.choose(),sep=";")
-head(table)
-attach(table)
-summary(table)
-na.omit(table$s14_4)
-na.omit(table)
-names(table) <- c("x","DistCode","SerialNumb","HousMembers","YearBuilt","NumbRooms","FloorSpace","Conso","tableYN","TypeCook","income")
-
-table$conso <- table$conso + 1
-table$conso <- log(table$conso)
-
-#  X
-#  mesto	DistCode
-#  rdb	SerialNumb
-#  brojclan	HousMembers
-#  s2	YearBuilt
-#  s3	NumbRooms
-#  s4	FloorSpace
-#  s14_4	Conso
-#  s9_1	tableY/N
-#  
-#  income	income
 
 
-table$YearBuilt <- table$YearBuilt - 1800
-summary(table)
+tab <- read_excel("~/GitHub/Applied/table.xlsx")
 
 
-LM1 <- lm()
+head(tab)
+attach(tab)
+summary(tab)
+na.omit(tab)
+View(tab)
+tab$conso <- log(tab$conso)
+
+
+
+tab$YearBuilt <- tab$YearBuilt - 1800
+summary(tab)
+
+
+
