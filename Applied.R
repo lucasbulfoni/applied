5aafcf89969b8a439bd6810228c28483e80e27d0
DLachiver <33716514+DLachiver@users.noreply.github.com>
2018-02-08 17:23:08 +0100

    trop de travai

--- Applied.R
+++ Applied.R
@@ -7,6 +7,31 @@ View(table)
 
 attach(table)
 
+
 modelLog <- lm(Lconso ~ income + HousMembers + NumbRooms + FloorSpace + ElecYN + ElecCookYN )
 summary(modelLog)
 
+model <- lm(Conso ~ income + HousMembers + NumbRooms + FloorSpace + ElecYN + ElecCookYN )
+summary(model)
+
+FS2 <- FloorSpace^2
+
+modelLog2 <- lm(Lconso ~ income + HousMembers + NumbRooms + FloorSpace + FS2 + ElecYN + ElecCookYN )
+summary(modelLog2)
+
+logInc <- log(income)
+
+modelLog3 <- lm(Lconso ~ income + HousMembers + NumbRooms + FloorSpace + FS2 + ElecYN + ElecCookYN + logInc)
+summary(modelLog3)
+
+sampleA<-subset(table, DistCode<100)
+SampleB<- subset (table, DistCode>=100)
+
+summary(sampleA)
+summary(SampleB)
+
+lmA <- lm (Lconso ~ FloorSpace+NumbRooms+income+ElecYN+ElecCookYN+Age+TableYN+HousMembers , data=sampleA)
+summary(lmA)
+
+lmB <- lm (Lconso ~ FloorSpace+NumbRooms+income+ElecYN+ElecCookYN+Age+TableYN+HousMembers , data=SampleB)
+summary(lmB)
